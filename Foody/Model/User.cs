b5f92c0bc6b680a8619c22daf1a6431f0d77426a
YONGHO KIM <kimyon@deakin.edu.au>
2019-09-11 18:30:38 +1000

    Change UserModel

---
+++ Foody/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Foody.Model
+{
+    public class User
+    {
+       
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Unit { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string State { get; set; }
+        public string Postcode { get; set; }
+
+
+    }
+}
+
+
+    
