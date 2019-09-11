181a2b79540aa2c574b4af4492694ad12eaa5df4
YONGHO KIM <kimyon@deakin.edu.au>
2019-09-11 19:08:08 +1000

    Connect with API and can save User data

--- Foody/RegistrationPage.xaml.cs
+++ Foody/RegistrationPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
+
+using Newtonsoft.Json;
+using System.Net.Http;
 using Xamarin.Forms.Xaml;
+using Foody.Data;
+using Foody.Model;
 
 namespace Foody
 {
@@ -15,6 +17,39 @@ namespace Foody
         public RegistrationPage()
         {
             InitializeComponent();
+        }
+
+        private async void SignUpbtn(object sender, EventArgs e)
+        {
+            if(u_PW.Text != u_PWC.Text)
+            {
+                await DisplayAlert("Failed", "Password and Confirm Password are different", "OK");
+            }
+            else
+            {
+                User user = new User();
+
+                user.First_Name = u_FNAME.Text;
+                user.Last_Name = u_LNAME.Text;
+                user.Email = u_Email.Text;
+                user.Password = u_PW.Text;
+                user.Phone = u_PhoneNumber.Text;
+                user.Unit = u_Unit.Text;
+                user.Street = u_Street.Text;
+                user.Suburb = u_Suburb.Text;
+                user.State = u_State.Text;
+                user.Postcode = u_Postcode.Text;
+
+
+
+                UserData userData = new UserData();
+                var data = await userData.PostDataAsync(user);
+
+                await DisplayAlert("Success", "Your data has been added", "OK");
+
+                await Navigation.PopAsync(); //error here
+            }
+
         }
     }
 }
