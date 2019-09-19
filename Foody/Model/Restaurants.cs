aed3111207a74109cb0a18cebb5d87ec1c0a1019
Anh Vuong <anhvvuong0212@gmail.com>
2019-09-20 09:47:49 +1000

    FINISH DESIGNRESTAURANT PAGE

--- Foody/Model/Restaurants.cs
+++ Foody/Model/Restaurants.cs
@@ -12,15 +12,17 @@ namespace Foody.Model
         public string R_Title { get; set; }
         public string R_Description { get; set; }
         public string R_BgImg { get; set; }
+        public string R_Logo { get; set; }
         
         //address
-        public int R_Unit { get; set; }
-        public string R_Street { get; set; }
-        public string R_Suburb { get; set; }
-        public string R_State { get; set; }
-        public int R_Postcode { get; set; }
-        public int R_ContactNumber { get; set; }
-        public string R_TradingHours { get; set; }
+        public string R_Address { get; set; }
+        //public int R_Unit { get; set; }
+        //public string R_Street { get; set; }
+        //public string R_Suburb { get; set; }
+        //public string R_State { get; set; }
+        //public int R_Postcode { get; set; }
+        //public int R_ContactNumber { get; set; }
+        //public string R_TradingHours { get; set; }
 
 
         public Restaurants()
@@ -28,18 +30,19 @@ namespace Foody.Model
 
         }
 
-        public Restaurants(int id, string title, string description, int unit, string street, string suburb, string state, int postcode, int phone, string tradinghour)
+        public Restaurants(int id, string title, string description, string address)
         {
             R_ID = id;
             R_Title = title;
             R_Description = description;
-            R_Unit = unit;
-            R_Street = street;
-            R_Suburb = suburb;
-            R_State = state;
-            R_Postcode = postcode;
-            R_ContactNumber = phone;
-            R_TradingHours = tradinghour;
+            R_Address = address;
+            //R_Unit = unit;
+            //R_Street = street;
+            //R_Suburb = suburb;
+            //R_State = state;
+            //R_Postcode = postcode;
+            //R_ContactNumber = phone;
+            //R_TradingHours = tradinghour;
 
         }
 
